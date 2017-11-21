df99a19ca39c6cc8dff2d1cdf4c024d892cf7bd5
Murali Nutalapati <info@muraliprashanth.me>
2017-11-21 10:53:31 +0530

    adds eslint config to the project

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const express = require("express");
+const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// connect to mongoose
+mongoose.connect('mongodb://localhost/video-dev', {
+  useMongoClient: true,
+}).then(() => { console.log('mongodb connected..!!'); }).catch(err => console.log(`error occured: ${err}`));
+
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
+  defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
 
 const port = 5000;
 
-app.listen(port, function() {
-  console.log("server started on port: " + port);
+app.listen(port, () => {
+  console.log(`server started on port: ${port}`);
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.render('index', {
-    title: "Welcome"
+    title: 'Welcome',
   });
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
   res.render('about');
 });
